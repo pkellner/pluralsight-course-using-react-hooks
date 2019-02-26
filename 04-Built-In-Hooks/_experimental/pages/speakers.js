83bba96a7a8fc10d0f057a79b8ac960f1dfff207
pkellner <peter@peterkellner.net>
2019-02-26 10:37:16 -0800

    wip

--- 04-Built-In-Hooks/_experimental/pages/speakers.js
+++ 04-Built-In-Hooks/_experimental/pages/speakers.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import SpeakerCard from "../src/speakerCard";
 
 const Speakers = () => {
   const [speakers, setSpeakers] = useState([]);
@@ -10,37 +9,69 @@ const Speakers = () => {
   const [searchText, setSearchText] = useState("");
   const [speakingSaturday, setSpeakingSaturday] = useState(true);
   const [speakingSunday, setSpeakingSunday] = useState(true);
+  const [serverSideFilter, setServerSideFilter] = useState(true);
 
-  useEffect(() => {
-    let mounted = true;
-    let source = axios.CancelToken.source();
-    axios
-      .get("http://localhost:4000/speakers", {
-        cancelToken: source.token
-      })
-      .then(a => {
-        if (mounted) {
-          setSpeakers(a.data);
-          setIsLoading(false);
-        }
-      })
-      .catch(function(e) {
-        if (mounted) {
-          setIsLoading(false);
-          setIsError(true);
-          setErrorMessage(e.message);
-        }
-      });
-    return function() {
-      mounted = false;
-      source.cancel("Cancelling in cleanup");
-    };
-  }, []);
+  function new1(rec) {
+    if (
+      (speakingSaturday && rec.speakingSaturday) ||
+      (speakingSunday && rec.speakingSunday)
+    ) {
+      return (
+        searchText.length === 0 ||
+        `${rec.firstName} ${rec.lastName}`
+          .toUpperCase()
+          .indexOf(searchText.toUpperCase()) !== -1
+      );
+    }
+    return false;
+  }
 
-  if (isError) return <div>Error: {errorMessage}</div>;
-  if (isLoading) return <div>loading...</div>;
+  useEffect(
+    () => {
+      let mounted = true;
+      let source = axios.CancelToken.source();
+      setIsLoading(true);
+      axios
+        .get("http://localhost:4000/speakers", {
+          cancelToken: source.token
+        })
+        .then(a => {
+          if (mounted) {
+            if (serverSideFilter) {
+              const speakerData = a.data;
+              const speakerDataFilter = speakerData.filter(new1);
+              setSpeakers(speakerDataFilter);
+            } else {
+              setSpeakers(a.data);
+            }
+            setIsLoading(false);
+          }
+        })
+        .catch(e => {
+          if (mounted) {
+            setIsLoading(false);
+            setIsError(true);
+            setErrorMessage(e.message);
+          }
+        });
+      return () => {
+        mounted = false;
+        source.cancel("Cancelling in cleanup");
+      };
+    },
+    serverSideFilter ? [searchText, speakingSaturday] : []
+  );
 
-  function speakingDays(speaker) {
+  if (isError)
+    return (
+      <div>
+        Error: {errorMessage} (likely json-server not running. To run: "npm run
+        json-server"
+      </div>
+    );
+  // if (isLoading) return <div>loading...</div>;
+
+  const speakingDays = speaker => {
     if (speaker.speakingSaturday && speaker.speakingSunday)
       return "Speaking Saturday and Sunday";
     if (speaker.speakingSaturday && !speaker.speakingSunday)
@@ -49,15 +80,38 @@ const Speakers = () => {
       return "Speaking Sunday";
     if (!speaker.speakingSaturday && !speaker.speakingSunday)
       return "Not Speaking";
-  }
+  };
 
-  function handleChangeSaturday() {
+  const filterData = rec => {
+    if (
+      (speakingSaturday && rec.speakingSaturday) ||
+      (speakingSunday && rec.speakingSunday)
+    ) {
+      return (
+        searchText.length === 0 ||
+        `${rec.firstName} ${rec.lastName}`
+          .toUpperCase()
+          .indexOf(searchText.toUpperCase()) !== -1
+      );
+    }
+    return false;
+  };
+
+  const handleChangeSaturday = () => {
     setSpeakingSaturday(!speakingSaturday);
-  }
+  };
 
-  function handleChangeSunday() {
+  const handleChangeSunday = () => {
     setSpeakingSunday(!speakingSunday);
-  }
+  };
+
+  // THIS DOES NOT WORK RIGHT, NEED TO MAKE SEPARATE LOAD FROM SERVER, LOAD FROM CLIENT WIHTOUT CHECKBOX TOGGLE
+  // function handleChangeServerSideFilter() {
+  //   setSearchText("");
+  //   setSpeakingSaturday(true);
+  //   setSpeakingSunday(true);
+  //   setServerSideFilter(!serverSideFilter);
+  // }
 
   return (
     <div className="container">
@@ -99,25 +153,29 @@ const Speakers = () => {
               return setSearchText(e.target.value);
             }}
           />
+          {/*<div>&nbsp;&nbsp;</div>*/}
+          {/*<div className="form-check-inline">*/}
+            {/*<label className="form-check-label">*/}
+              {/*<input*/}
+                {/*type="checkbox"*/}
+                {/*className="form-check-input"*/}
+                {/*onChange={handleChangeServerSideFilter}*/}
+                {/*checked={serverSideFilter}*/}
+              {/*/>*/}
+              {/*Filter From Server*/}
+            {/*</label>*/}
+          {/*</div>*/}
+        </div>
+        <div className="text-lg-left">
+          <span>
+            <i>&nbsp;&nbsp;{isLoading ? "loading..." : ""}&nbsp;&nbsp;</i>
+          </span>
         </div>
       </div>
       <div className="row">
         <div className="card-deck">
           {speakers
-            .filter(function(rec) {
-              if (
-                (speakingSaturday && rec.speakingSaturday) ||
-                (speakingSunday && rec.speakingSunday)
-              ) {
-                return (
-                  searchText.length === 0 ||
-                  `${rec.firstName} ${rec.lastName}`
-                    .toUpperCase()
-                    .indexOf(searchText.toUpperCase()) !== -1
-                );
-              }
-              return false;
-            })
+            .filter(!serverSideFilter ? new1 : () => true)
             .map(speaker => (
               <div
                 className="card col-4 cardmin margintopbottom20"
