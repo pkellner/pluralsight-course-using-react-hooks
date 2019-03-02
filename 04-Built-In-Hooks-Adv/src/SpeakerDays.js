b71ded94f4ffedee9bede8b6863f403787d8e2c7
pkellner <peter@peterkellner.net>
2019-03-02 08:23:34 -0800

    fix

--- 04-Built-In-Hooks-Adv/src/SpeakerDays.js
+++ 04-Built-In-Hooks-Adv/src/SpeakerDays.js
@@ -1,19 +1,11 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 
 const SpeakerDays = ({ show, saturday, sunday }) => {
-  const numRendersRef = useRef(1);
-  useEffect(() => {
-    numRendersRef.current++;
-    console.log(`SpeakerDays.js:numRendersRef:${numRendersRef.current} ${Math.random().toString(36).substring(7)}`);
-  });
-
   if (saturday && sunday) return "Speaking Saturday and Sunday";
   if (saturday && !sunday) return "Speaking Saturday";
   if (!saturday && sunday) return "Speaking Sunday";
   if (!saturday && !sunday) return "Not Speaking";
   return show === true ? <React.Fragment>{speakingDays}</React.Fragment> : null;
 };
-
-//{/*<div>{show} {saturday} {sunday}</div>*/}
 
 export default SpeakerDays;
