0d766c017cc444c459f3664cb80c8f5409b5d83f
pkellner <peter@peterkellner.net>
2019-03-11 20:04:39 -0700

    ip

---
+++ 02-Basic-React-Hooks/clip-c-adding-bootstrap-style/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./Home";
+import Speakers from "./Speakers";
+
+const pageToShow = pageName => {
+  if (pageName === "Home") return <Home />;
+  if (pageName === "Speakers") return <Speakers />;
+  return <div>Not Found</div>;
+};
+
+const App = ({ pageName }) => {
+  return <div>{pageToShow(pageName)}</div>;
+};
+
+export default App;
---
+++ 02-Basic-React-Hooks/clip-c-adding-bootstrap-style/src/Speakers.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect, useContext } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../static/site.css";
+import { Header } from "../src/Header";
+import { Menu } from "../src/Menu";
+
+const Speakers = () => {
+
+
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <div className="container" >
+        <h2>Speakers</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Speakers;
