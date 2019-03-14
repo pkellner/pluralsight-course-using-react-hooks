1584aabff6e89357cfd0a1d38035913816caf590
pkellner <peter@peterkellner.net>
2019-03-14 09:24:17 -0400

    wip

--- 02-Basic-React-Hooks/clip-c-adding-bootstrap-style/src/Speakers.js
+++ 02-Basic-React-Hooks/clip-c-adding-bootstrap-style/src/Speakers.js
@@ -19,12 +19,37 @@ const Speakers = ({}) => {
     setSpeakingSunday(!speakingSunday);
   };
 
+  const speakerList = [
+    {
+      id: 1124,
+      firstName: "Douglas",
+      lastName: "Crockford",
+      sat: true,
+      sun: false
+    },
+    {
+      id: 8367,
+      firstName: "Gayle",
+      lastName: "Laakmann McDowell",
+      sat: false,
+      sun: true
+    },
+    { id: 823, firstName: "Kevin", lastName: "Nilson", sat: true, sun: true },
+    {
+      id: 8590,
+      firstName: "Chris",
+      lastName: "Richardson",
+      sat: false,
+      sun: true
+    }
+  ];
+
   return (
     <div>
       <Header />
       <Menu />
       <div className="container">
-        <div className="btn-toolbar  margintopbottom ">
+        <div className="btn-toolbar  margintopbottom5 checkbox-bigger">
           <div className="form-check-inline">
             <label className="form-check-label">
               <input
@@ -47,64 +72,30 @@ const Speakers = ({}) => {
               Sunday Speakers
             </label>
           </div>
-
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <div className="input-group-text" id="btnGroupAddon">
-                @
-              </div>
-            </div>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search by name"
-              onChange={e => {
-                return setSearchText(e.target.value);
-              }}
-            />
-          </div>
         </div>
         <div className="row">
           <div className="card-deck">
-            <div className="card col-4 cardmin margintopbottom20" key={1123}>
-              <img
-                className="card-img-top"
-                src={`/static/speakers/Speaker-1124.jpg`}
-              />
-              <div className="card-body">
-                <h4 className="card-title">Douglas Crockford</h4>
-                <p className="card-text">JSON guy</p>
-                <p>
-                  <i />
-                </p>
-              </div>
-            </div>
-            <div className="card col-4 cardmin margintopbottom20" key={8367}>
-              <img
-                className="card-img-top"
-                src={`/static/speakers/Speaker-8367.jpg`}
-              />
-              <div className="card-body">
-                <h4 className="card-title">Gayle Laakmann McDowell</h4>
-                <p className="card-text">Author and Speaker</p>
-                <p>
-                  <i />
-                </p>
-              </div>
-            </div>
-            <div className="card col-4 cardmin margintopbottom20" key={8590}>
-              <img
-                className="card-img-top"
-                src={`/static/speakers/Speaker-8590.jpg`}
-              />
-              <div className="card-body">
-                <h4 className="card-title">Chris Richardson</h4>
-                <p className="card-text">Microservices Expert</p>
-                <p>
-                  <i />
-                </p>
-              </div>
-            </div>
+            {speakerList
+              .filter(({ sat, sun }) => (speakingSaturday && sat) || (speakingSunday && sun))
+              .map(({ id, firstName, lastName }) => {
+                return (
+                  <div
+                    className="card col-4 cardmin margintopbottom20"
+                    key={id}
+                  >
+                    <img
+                      className="card-img-top"
+                      src={`/static/speakers/Speaker-${id}.jpg`}
+                      alt="{firstName} {lastName}"
+                    />
+                    <div className="card-body">
+                      <h4 className="card-title">
+                        {firstName} {lastName}
+                      </h4>
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
