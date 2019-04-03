14a0889cfd12f46bc4c952f0028e1d7f2fb9c489
pkellner <peter@peterkellner.net>
2019-04-03 16:46:12 -0700

    mode 3 work

---
+++ 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip2-React-Hooks-eslint-and-Usage/readme.txt
@@ -0,0 +1,3 @@
+The only update for clip 2 is to the package.json file and adding .eslintrc.json.
+
+Both of these changes are reflected in the first source for this module (clip4). You will find those two files there.
---
+++ 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip5-the-useContext-hook/src/Speakers.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect} from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../static/site.css";
+import { Header } from "../src/Header";
+import { Menu } from "../src/Menu";
+import SpeakerData from "./SpeakerData";
+import SpeakerDetail from "./SpeakerDetail";
+
+const Speakers = ({}) => {
+  const [speakingSaturday, setSpeakingSaturday] = useState(true);
+  const [speakingSunday, setSpeakingSunday] = useState(true);
+
+  const [speakerList, setSpeakerList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    new Promise(function(resolve) {
+      setTimeout(function() {
+        resolve();
+      }, 1000);
+    }).then(() => {
+      setIsLoading(false);
+      const speakerListServerFilter = SpeakerData.filter(({ sat, sun }) => {
+        return (speakingSaturday && sat) || (speakingSunday && sun);
+      });
+      setSpeakerList(speakerListServerFilter);
+    });
+    return () => {
+      console.log("cleanup");
+    };
+  }, []); // [speakingSunday, speakingSaturday]);
+
+  const handleChangeSaturday = () => {
+    setSpeakingSaturday(!speakingSaturday);
+  };
+
+  const speakerListFiltered = isLoading
+    ? []
+    : speakerList
+        .filter(
+          ({ sat, sun }) => (speakingSaturday && sat) || (speakingSunday && sun)
+        )
+        .sort(function(a, b) {
+          if (a.firstName < b.firstName) {
+            return -1;
+          }
+          if (a.firstName > b.firstName) {
+            return 1;
+          }
+          return 0;
+        });
+
+  const handleChangeSunday = () => {
+    setSpeakingSunday(!speakingSunday);
+  };
+
+  const heartFavoriteHandler = (e, favoriteValue) => {
+    e.preventDefault();
+    const sessionId = parseInt(e.target.attributes["data-sessionid"].value);
+    setSpeakerList(speakerList.map(item => {
+      if (item.id === sessionId) {
+        item.favorite = favoriteValue;
+        return item;
+      }
+      return item;
+    }));
+    //console.log("changing session favorte to " + favoriteValue);
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <div className="container">
+        <div className="btn-toolbar  margintopbottom5 checkbox-bigger">
+          <div className="hide">
+            <div className="form-check-inline">
+              <label className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  onChange={handleChangeSaturday}
+                  checked={speakingSaturday}
+                />
+                Saturday Speakers
+              </label>
+            </div>
+            <div className="form-check-inline">
+              <label className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  onChange={handleChangeSunday}
+                  checked={speakingSunday}
+                />
+                Sunday Speakers
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="card-deck">
+            {speakerListFiltered.map(
+              ({ id, firstName, lastName, bio, favorite }) => {
+                return (
+                  <SpeakerDetail
+                    key={id}
+                    id={id}
+                    favorite={favorite}
+                    onHeartFavoriteHandler={heartFavoriteHandler}
+                    firstName={firstName}
+                    lastName={lastName}
+                    bio={bio}
+                  />
+                );
+              }
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Speakers;
