7af8b3c6afe8329d32f87445640456c31d67f0bb
pkellner <peter@peterkellner.net>
2020-07-02 17:28:16 -0700

    working

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip6-using-useRef-Hook-onScroll/src/ImageToggleOnScroll.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip6-using-useRef-Hook-onScroll/src/ImageToggleOnScroll.js
@@ -3,39 +3,47 @@ import React, { useRef, useEffect, useState } from "react";
 const ImageTogglerOnScroll = ({ primaryImg, secondaryImg }) => {
   const imageRef = useRef(null);
   const [isLoading, setIsLoading] = useState(true);
-  
+
   const calcInView = () => {
-    if (!imageRef.current) return false;
     const rect = imageRef.current.getBoundingClientRect();
     return rect.top >= 0 && rect.bottom <= window.innerHeight;
   };
 
   useEffect(() => {
     window.addEventListener("scroll", scrollHandler);
+    
     // setIsLoading(false);
     // setInView(calcInView());
-    
+
     setTimeout(() => {
       setIsLoading(false);
       setInView(calcInView());
-    }, 0);
+    }, 1000);
+    
     return () => {
       window.removeEventListener("scroll", scrollHandler);
     };
-  },[]);
+  }, []);
 
   const [inView, setInView] = useState(false);
-  
+
   const scrollHandler = () => {
     setInView(calcInView());
   };
-  
-  return isLoading ? null : (
-    <img
-      src={inView ? secondaryImg : primaryImg}
-      alt="" ref={imageRef} width="200" height="200"
+
+  return <img
+      src={
+        isLoading
+          ? "https://i.gifer.com/4V0b.gif"
+          : inView
+          ? secondaryImg
+          : primaryImg
+      }
+      alt=""
+      ref={imageRef}
+      width="200"
+      height="200"
     />
-  );
   
 };
 
