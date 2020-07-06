c60e1945cc81007fc64fe39fad57290dd88e562f
pkellner <peter@peterkellner.net>
2020-07-05 18:02:02 -0700

    wip

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip6-using-useRef-Hook-onScroll/src/ImageToggleOnScroll.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip6-using-useRef-Hook-onScroll/src/ImageToggleOnScroll.js
@@ -3,54 +3,50 @@ import React, { useRef, useEffect, useState } from "react";
 const ImageTogglerOnScroll = ({ primaryImg, secondaryImg }) => {
   const imageRef = useRef(null);
   const [isLoading, setIsLoading] = useState(true);
-
+  
   const isInView = () => {
     const rect = imageRef.current.getBoundingClientRect();
     return rect.top >= 0 && rect.bottom <= window.innerHeight;
   };
-
+  
   useEffect(() => {
-  
-    console.log(`useEffect called ImageToggleOnScroll ${new Date()} ${primaryImg} ${isLoading}`);
-  
     window.addEventListener("scroll", scrollHandler);
-
-    // setIsLoading(false);
-    // setInView(isInView());
-
-    setTimeout(() => {
-      setIsLoading(false);
-      setInView(isInView());
-    }, 1000);
-
+    
+    setIsLoading(false);
+    setInView(isInView());
+    
+    // setTimeout(() => {
+    //   setIsLoading(false);
+    //   setInView(isInView());
+    // }, 1000);
+    
     return () => {
       window.removeEventListener("scroll", scrollHandler);
     };
-  });
-
+  },[]);
+  
   const [inView, setInView] = useState(false);
-
+  
   const scrollHandler = () => {
     setInView(isInView());
   };
   
-  console.log(`Rendering ImageToggleOnScroll ${new Date()} ${primaryImg} ${isLoading}`);
+  //console.log(`ImageToggleOnScroll.js isLoading:${isLoading}`);
   
-  return (
-    <img
-      src={
-        isLoading
-          ? "static/LoadingImage.gif"
-          : inView
-          ? secondaryImg
-          : primaryImg
-      }
-      alt=""
-      ref={imageRef}
-      width="200"
-      height="200"
-    />
-  );
+  return <img
+    src={
+      isLoading
+        ? "/static/Transparent.gif"
+        : inView
+        ? secondaryImg
+        : primaryImg
+    }
+    alt=""
+    ref={imageRef}
+    width="200"
+    height="200"
+  />
+  
 };
 
 export default ImageTogglerOnScroll;
