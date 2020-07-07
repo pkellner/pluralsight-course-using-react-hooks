98e4880cb9a381ee643d8288ebf51a36c03c952c
pkellner <peter@peterkellner.net>
2020-07-07 10:34:44 -0700

    working

---
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip6-using-useRef-Hook-onScroll/src/ImageToggleOnScroll.js
@@ -0,0 +1,32 @@
+ import React, {useRef, useEffect, useState} from "react";
+
+const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
+ 
+    const imageRef = useRef(null);
+    
+    const isInView = () => {
+        const rect = imageRef.current.getBoundingClientRect();
+        return rect.top >= 0 && rect.bottom <= window.innerHeight;
+    };
+    
+    const [inView,setInView] = useState(false);
+    
+    useEffect(() => {
+        window.addEventListener("scroll", scrollHandler);
+        return () => {
+            window.removeEventListener("scroll", scrollHandler);
+        };
+    },[]);
+    
+    const scrollHandler = () => {
+        setInView(isInView());
+    };
+
+    return (
+        <img src={inView ? secondaryImg : primaryImg}
+            alt="" ref={imageRef}
+        />
+    );
+};
+
+export default ImageToggleOnScroll;
---
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip7-using-useRef-Hook-onScroll/src/ImageToggleOnMouseOver.js
@@ -0,0 +1,21 @@
+import React, { useRef } from "react";
+
+const ImageTogglerOnMouseOver = ({ primaryImg, secondaryImg }) => {
+  const imageRef = useRef(null);
+
+  return (
+    <img
+      onMouseOver={() => {
+        imageRef.current.src = secondaryImg;
+      }}
+      onMouseOut={() => {
+        imageRef.current.src = primaryImg;
+      }}
+      src={primaryImg}
+      alt=""
+      ref={imageRef}
+    />
+  );
+};
+
+export default ImageTogglerOnMouseOver;
