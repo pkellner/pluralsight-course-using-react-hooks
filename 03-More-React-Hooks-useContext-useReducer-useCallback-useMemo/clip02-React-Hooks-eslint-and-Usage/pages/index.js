f6f8be68fa046639e8edaff5ff9aea73abe79797
pkellner <peter@peterkellner.net>
2020-07-07 17:47:31 -0700

    wip

--- 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip02-React-Hooks-eslint-and-Usage/pages/index.js
+++ 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip02-React-Hooks-eslint-and-Usage/pages/index.js
@@ -2,18 +2,24 @@ import React, { useEffect, useState } from "react";
 
 const InputElement = () => {
   
-  const [isLoading, setIsLoading] = useState(true);
+  //const [isLoading, setIsLoading] = useState(true);
   
   // THE BELOW CODE WILL CAUSE AN ERROR ON useState BECAUSE REACT HOOKS CAN NOT BE CALLED INSIDE A CONDITIONAL STATEMENT
-  // if (new Date() == "now") {
+  const random_boolean = Math.random() >= 0.5;
+  // if (random_boolean === true) {
   //   const [isLoading, setIsLoading] = useState(true);
+  // } else {
+  //   const [isLoading,setIsLoading] = useState(false);
   // }
+  const [isLoading,setIsLoading] = useState(random_boolean === true);
 
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
     }, 2000);
   });
+  
+  //return <div>{random_boolean === true ? "true" : "false"}</div>
 
   return isLoading ? (
     <div>loading...</div>
