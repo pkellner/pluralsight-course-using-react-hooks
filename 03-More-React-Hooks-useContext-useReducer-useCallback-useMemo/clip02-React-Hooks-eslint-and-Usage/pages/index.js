f5fcdd178669feb301b045e48125f978e6f9f7bd
pkellner <peter@peterkellner.net>
2020-07-08 12:26:46 -0700

    wip

--- 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip02-React-Hooks-eslint-and-Usage/pages/index.js
+++ 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip02-React-Hooks-eslint-and-Usage/pages/index.js
@@ -1,31 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
 const InputElement = () => {
   
-  //const [isLoading, setIsLoading] = useState(true);
-  
-  // THE BELOW CODE WILL CAUSE AN ERROR ON useState BECAUSE REACT HOOKS CAN NOT BE CALLED INSIDE A CONDITIONAL STATEMENT
   const random_boolean = Math.random() >= 0.5;
+  //const [isLoading,setIsLoading] = useState(true);
+  
+  const [isLoading,setIsLoading] = useState(random_boolean === true);
+  
   // if (random_boolean === true) {
-  //   const [isLoading, setIsLoading] = useState(true);
+  //   const [isLoading,setIsLoading] = useState(true);
   // } else {
   //   const [isLoading,setIsLoading] = useState(false);
   // }
-  const [isLoading,setIsLoading] = useState(random_boolean === true);
-
+  
   useEffect(() => {
     setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  });
+      setIsLoading(false)
+    },2000);
+  })
   
-  //return <div>{random_boolean === true ? "true" : "false"}</div>
-
-  return isLoading ? (
-    <div>loading...</div>
-  ) : (
-    <input placeholder="Enter Some Text" />
-  );
+  return isLoading ? <div>Loading...</div> : <input placeholder="Enter Some Text" />;
 };
 
 export default InputElement;
