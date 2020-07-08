13c340ae8f0e889eae97dcc56aace2a0c35329ec
pkellner <peter@peterkellner.net>
2020-07-08 16:31:28 -0700

    wip

--- 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip05-Preparing-React-Context-For-useContext/pages/bootstrap.js
+++ 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip05-Preparing-React-Context-For-useContext/pages/bootstrap.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 
-import "bootstrap/dist/css/bootstrap.css";
-//import "/static/site.css";
-
 class bootstrap extends Component {
   render() {
     return <div>
--- 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip05-Preparing-React-Context-For-useContext/src/Speakers.js
+++ 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip05-Preparing-React-Context-For-useContext/src/Speakers.js
@@ -1,7 +1,4 @@
 import React, { useState, useEffect} from "react";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../static/site.css";
 import { Header } from "../src/Header";
 import { Menu } from "../src/Menu";
 import SpeakerData from "./SpeakerData";
