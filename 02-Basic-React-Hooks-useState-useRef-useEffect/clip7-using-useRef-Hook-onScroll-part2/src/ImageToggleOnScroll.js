cee906164f987d86a5c54a4dac3a68be98f6882d
pkellner <peter@peterkellner.net>
2020-07-10 16:44:06 -0700

    wip

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip7-using-useRef-Hook-onScroll-part2/src/ImageToggleOnScroll.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip7-using-useRef-Hook-onScroll-part2/src/ImageToggleOnScroll.js
@@ -1,11 +1,10 @@
 import React, { useRef, useEffect, useState } from "react";
 
 const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
-  
   const imageRef = useRef(null);
-
-  const [isLoading, setIsLoading] = useState(true);
-
+  
+  const [isLoading,setIsLoading] = useState(true);
+  
   const isInView = () => {
     const rect = imageRef.current.getBoundingClientRect();
     return rect.top >= 0 && rect.bottom <= window.innerHeight;
@@ -25,10 +24,12 @@ const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
   const scrollHandler = () => {
     setInView(isInView());
   };
-
+  
   return (
     <img
-      src={inView ? secondaryImg : primaryImg}
+      src={
+        isLoading ? "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" // 1x1gif
+          : inView ? secondaryImg : primaryImg}
       alt=""
       ref={imageRef}
       width="200"
