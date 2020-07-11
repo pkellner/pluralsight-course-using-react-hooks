a37421e4f76ec89096c0c5a55d3582d3cfec9e6c
pkellner <peter@peterkellner.net>
2020-07-11 16:48:26 -0700

    working

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip7-using-useRef-Hook-onScroll-part3/pages/ImageChangeOnScroll.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip7-using-useRef-Hook-onScroll-part3/pages/ImageChangeOnScroll.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImageToggleOnScroll from "../src/ImageToggleOnScroll";
 
 const ImageChangeOnScroll = () => {
-  
+  const [currentSpeakerId, setCurrentSpeakerId] = useState(0);
+  const [mouseEventCnt, setMouseEventCnt] = useState(0);
+
+  useEffect(() => {
+    //if (currentSpeakerId != 0) {
+      window.document.title = `SpeakerId: ${currentSpeakerId}`;
+      console.log(`useEffect: setting title to ${currentSpeakerId}`);
+    //}
+  },[currentSpeakerId]);
+
   return (
     <div>
+      <span>mouseEventCnt: {mouseEventCnt}</span>
       {[1124, 187, 823, 1269, 1530].map((speakerId) => {
         return (
-          <div key={speakerId}>
+          <div
+            key={speakerId}
+            onMouseOver={() => {
+              console.log(`Enter:${speakerId}`);
+              setCurrentSpeakerId(speakerId);
+              setMouseEventCnt(mouseEventCnt + 1);
+            }}
+          >
             <ImageToggleOnScroll
               primaryImg={`/static/speakers/bw/Speaker-${speakerId}.jpg`}
               secondaryImg={`/static/speakers/Speaker-${speakerId}.jpg`}
--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip7-using-useRef-Hook-onScroll-part3/src/ImageToggleOnScroll.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip7-using-useRef-Hook-onScroll-part3/src/ImageToggleOnScroll.js
@@ -3,9 +3,7 @@ import React, { useRef, useEffect, useState } from "react";
 const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
   const imageRef = useRef(null);
   
-  const [isLoading, setIsLoading] = useState(true);
-  
-  const [isThumbnail, setIsThumbnail] = useState(true);
+  const [isLoading,setIsLoading] = useState(true);
   
   const isInView = () => {
     const rect = imageRef.current.getBoundingClientRect();
@@ -16,42 +14,27 @@ const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
   
   useEffect(() => {
     setIsLoading(false);
+    setInView(isInView());
     window.addEventListener("scroll", scrollHandler);
     return () => {
       window.removeEventListener("scroll", scrollHandler);
     };
   }, []);
   
-  useEffect(() => {
-    setInView(isInView());
-  }, [isThumbnail]);
-  
   const scrollHandler = () => {
     setInView(isInView());
   };
   
   return (
-    <div>
-      <img
-        src={
-          isLoading
-            ? "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" // 1x1gif
-            : inView
-            ? secondaryImg
-            : primaryImg
-        }
-        alt=""
-        ref={imageRef}
-        width={isThumbnail ? "100" : "300"}
-        height={isThumbnail ? "100" : "300"}
-      />
-      <input
-        type="checkbox"
-        onChange={() => {
-          setIsThumbnail(!isThumbnail);
-        }}
-      />
-    </div>
+    <img
+      src={
+        isLoading ? "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" // 1x1gif
+          : inView ? secondaryImg : primaryImg}
+      alt=""
+      ref={imageRef}
+      width="200"
+      height="200"
+    />
   );
 };
 
