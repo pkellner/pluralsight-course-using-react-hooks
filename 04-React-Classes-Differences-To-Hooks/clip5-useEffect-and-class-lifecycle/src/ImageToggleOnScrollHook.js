a2c7bf08cf070331e5b85dab4865ad3891e4ce68
pkellner <peter@peterkellner.net>
2020-07-17 14:22:38 -0700

    working

--- 04-React-Classes-Differences-To-Hooks/clip5-useEffect-and-class-lifecycle/src/ImageToggleOnScrollHook.js
+++ 04-React-Classes-Differences-To-Hooks/clip5-useEffect-and-class-lifecycle/src/ImageToggleOnScrollHook.js
@@ -2,16 +2,16 @@ import React, { useRef, useEffect, useState } from "react";
 
 const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
   const imageRef = useRef(null);
-  
-  const [isLoading,setIsLoading] = useState(true);
-  
+
+  const [isLoading, setIsLoading] = useState(true);
+
   const isInView = () => {
     const rect = imageRef.current.getBoundingClientRect();
     return rect.top >= 0 && rect.bottom <= window.innerHeight;
   };
-  
+
   const [inView, setInView] = useState(false);
-  
+
   useEffect(() => {
     setIsLoading(false);
     setInView(isInView());
@@ -20,16 +20,20 @@ const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
       window.removeEventListener("scroll", scrollHandler);
     };
   }, []);
-  
+
   const scrollHandler = () => {
     setInView(isInView());
   };
-  
+
   return (
     <img
       src={
-        isLoading ? "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" // 1x1gif
-          : inView ? secondaryImg : primaryImg}
+        isLoading
+          ? "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" // 1x1gif
+          : inView
+          ? secondaryImg
+          : primaryImg
+      }
       alt=""
       ref={imageRef}
       width="200"
