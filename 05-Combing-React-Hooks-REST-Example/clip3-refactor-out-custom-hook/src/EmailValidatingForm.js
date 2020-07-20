3cb24f1f8e308d8ae4e8b38a531dee0746b799b8
pkellner <peter@peterkellner.net>
2020-07-20 13:50:42 -0700

    wip

--- 05-Combing-React-Hooks-REST-Example/clip3-refactor-out-custom-hook/src/EmailValidatingForm.js
+++ 05-Combing-React-Hooks-REST-Example/clip3-refactor-out-custom-hook/src/EmailValidatingForm.js
@@ -2,6 +2,7 @@ import React, { useState, useReducer, useEffect } from "react";
 import useInterval from "./useInterval";
 
 function EmailValidatingForm() {
+  
   const validateEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
