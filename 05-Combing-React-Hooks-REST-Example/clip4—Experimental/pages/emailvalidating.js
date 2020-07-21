f35a78eabe51770dfbbf445e2caad17367e55639
pkellner <peter@peterkellner.net>
2020-07-21 09:44:34 -0700

    wip

---
+++ 05-Combing-React-Hooks-REST-Example/clip4—Experimental/pages/emailvalidating.js
@@ -0,0 +1,8 @@
+import React from "react";
+import EmailValidatingForm from "../src/EmailValidatingForm";
+
+function emailvalidating() {
+  return <EmailValidatingForm />;
+}
+
+export default emailvalidating;
