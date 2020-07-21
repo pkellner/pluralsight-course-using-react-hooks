4d7240d7c853eec38fc3c6185421f840abec8ad2
pkellner <peter@peterkellner.net>
2020-07-20 17:58:27 -0700

    mm5-clip3 complete

--- 05-Combing-React-Hooks-REST-Example/clip3-refactor-out-custom-hook-final/pages/emailvalidating.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import EmailValidatingForm from "../src/EmailValidatingForm";
-
-function emailvalidating() {
-  return <EmailValidatingForm />;
-}
-
-export default emailvalidating;
--- 05-Combing-React-Hooks-REST-Example/clip4-adding-useAxiosFetch-custom-hook/src/EmailValidatingForm.js
+++ 05-Combing-React-Hooks-REST-Example/clip4-adding-useAxiosFetch-custom-hook/src/EmailValidatingForm.js
@@ -1,25 +1,9 @@
 import React, { useState, useReducer, useEffect } from "react";
-import useInterval from "./useInterval";
+import useEmailValidation from './useEmailValidation';
 
 function EmailValidatingForm() {
-  const validateEmail = (email) => {
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  };
-  const [emailValid, setEmailValid] = useState(false);
-  const emailReducer = (state, action) => {
-    const isValidEmail = validateEmail(action);
-    setEmailValid(isValidEmail);
-    return action;
-  };
-
-  const [email, setEmail] = useReducer(emailReducer, "");
-  const maxSeconds = 30;
-  const [count, setCount] = useState(maxSeconds);
-
-  useInterval(() => {
-    setCount(count - 1);
-  }, 1000);
+  
+  const {setEmail, count, email, emailValid, setCount} = useEmailValidation(30);
 
   return (
     <div className="container"><br />
