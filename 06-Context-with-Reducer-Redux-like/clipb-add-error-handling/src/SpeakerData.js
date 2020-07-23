e9850f3a3a70ce97aea64d228e3eb130311eb0d4
pkellner <peter@peterkellner.net>
2020-07-23 14:54:19 -0700

    wip

---
+++ 06-Context-with-Reducer-Redux-like/clipb-add-error-handling/src/SpeakerData.js
@@ -0,0 +1,143 @@
+const speakerData = [
+  {
+    id: 1530,
+    firstName: 'Tammy',
+    lastName: 'Baker',
+    sat: false,
+    sun: true,
+    favorite: true,
+    bio:
+      'Tammy has held a number of executive and management roles over the past 15 years, including VP engineering Roles at Molekule Inc., Cantaloupe Systems, E-Color, and Untangle Inc.',
+  },
+  {
+    id: 5996,
+    firstName: 'Craig',
+    lastName: 'Berntson',
+    sat: true,
+    sun: true,
+    favorite: false,
+    bio:
+      'Craig has a passion for community and helping other developers improve their skills. He writes the column "Software Gardening" in DotNet Curry Magazine and is the co-author of "Continuous Integration in .NET" available from Manning.',
+  },
+  {
+    id: 10803,
+    firstName: 'Eugene',
+    lastName: 'Chuvyrov',
+    sat: true,
+    sun: false,
+    favorite: false,
+    bio:
+      'Eugene Chuvyrov is  a Senior Cloud Architect at Microsoft. He works directly with both startups and enterprises to enable their solutions in Microsoft cloud, and to make Azure better as a result of this work with partners.',
+  },
+  {
+    id: 1124,
+    firstName: 'Douglas',
+    lastName: 'Crockford',
+    sat: true,
+    sun: false,
+    favorite: true,
+    bio:
+      'Douglas Crockford discovered the JSON Data Interchange Format. He is also the author of _JavaScript: The Good Parts_. He has been called a guru, but he is actually more of a mahatma.',
+  },
+  {
+    id: 41808,
+    firstName: 'Paul',
+    lastName: 'Everitt',
+    sat: true,
+    sun: true,
+    favorite: false,
+    bio:
+      'Paul is the PyCharm and WebStorm Developer Advocate at JetBrains. Before that, Paul was a partner at Agendaless Consulting and co-founder of Zope Corporation, taking the first open source application server through $14M of funding.',
+  },
+  {
+    id: 1269,
+    firstName: 'Arun',
+    lastName: 'Gupta',
+    sat: false,
+    sun: true,
+    favorite: true,
+    bio:
+      'Arun Gupta is a Principal Open Source Technologist at Amazon Web Services. He has built and led developer communities for 12+ years at Sun, Oracle, Red Hat and Couchbase.',
+  },
+  {
+    id: 1725,
+    firstName: 'Brad',
+    lastName: 'Irby',
+    sat: true,
+    sun: false,
+    favorite: false,
+    bio:
+      'Brad is an accomplished .NET software architect specializing in Domain Driven Design and Event Driven Architectures.',
+  },
+  {
+    id: 18805,
+    firstName: 'Mickey W.',
+    lastName: 'Mantle',
+    sat: true,
+    sun: true,
+    favorite: false,
+    bio:
+      'Mickey has been developing software systems and products for over 40 years, as a systems programmer, Tech Lead, Manager, VP Engineering, CTO, COO, and now CEO/CTO of his own company.',
+  },
+  {
+    id: 8367,
+    firstName: 'Gayle Laakmann',
+    lastName: 'McDowell',
+    sat: true,
+    sun: false,
+    favorite: false,
+    bio:
+      'Gayle Laakmann McDowell is the founder and CEO of CareerCup.com and the author of three best selling books.',
+  },
+  {
+    id: 187,
+    firstName: 'Dave',
+    lastName: 'Nielsen',
+    sat: true,
+    sun: true,
+    favorite: false,
+    bio:
+      'As Head of Ecosystem Programs, Dave uses emerging technologies and open source projects like Microservices, Serverless & Kubernetes to bring the magic of Redis to the broader community.',
+  },
+  {
+    id: 823,
+    firstName: 'Kevin',
+    lastName: 'Nilson',
+    sat: false,
+    sun: true,
+    favorite: true,
+    bio:
+      "Team Lead of the Chromecast Technical Solutions Engineer team, a Java Champion and three time JavaOne Rock Star Presenter. Kevin has spoken at conferences such as JavaOne, Devoxx, JAX, O'Reilly Fluent, Silicon Valley Code Camp, JAX and HTML5DevConf.",
+  },
+  {
+    id: 8590,
+    firstName: 'Chris',
+    lastName: 'Richardson',
+    sat: true,
+    sun: false,
+    favorite: false,
+    bio:
+      'Chris Richardson is a developer and architect. He is a Java Champion, a JavaOne rock star and the author of POJOs in Action, which describes how to build enterprise Java applications with frameworks such as Spring and Hibernate.',
+  },
+  {
+    id: 10808,
+    firstName: 'Antony',
+    lastName: 'Ross',
+    sat: true,
+    sun: false,
+    favorite: true,
+    bio:
+      'Antony Ross is a consultant specializing in data science and machine learning applied to sports performance. He has worked closely with USC and UCLA and is presently involved with the Recurse Center in New York researching deep learning and voice recognition.',
+  },
+  {
+    id: 6548,
+    firstName: 'Steve',
+    lastName: 'Souders',
+    sat: true,
+    sun: false,
+    favorite: true,
+    bio:
+      "Steve works at SpeedCurve on the interplay between performance and design. He previously served as Google's Head Performance Engineer, Chief Performance Yahoo!, and Chief Performance Officer at Fastly.",
+  },
+];
+export default speakerData;
