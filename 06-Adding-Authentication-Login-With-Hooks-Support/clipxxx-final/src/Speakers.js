a5ef06056a63a35c6c5127649e4c74935e78f735
pkellner <peter@peterkellner.net>
2020-07-23 13:59:40 -0700

    working

---
+++ 06-Adding-Authentication-Login-With-Hooks-Support/clipxxx-final/src/Speakers.js
@@ -0,0 +1,120 @@
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  useReducer,
+  useCallback,
+  useMemo,
+} from 'react';
+
+import { Header } from '../src/Header';
+import { Menu } from '../src/Menu';
+import SpeakerData from './SpeakerData';
+import SpeakerDetail from './SpeakerDetail';
+import { ConfigContext } from './App';
+import speakersReducer from './speakersReducer';
+import useSpeakerDataManager from './useSpeakerDataManager';
+
+const Speakers = ({}) => {
+  const [speakingSaturday, setSpeakingSaturday] = useState(true);
+  const [speakingSunday, setSpeakingSunday] = useState(true);
+  const context = useContext(ConfigContext);
+
+  const { isLoading, speakerList, updateSpeakerRecord } = useSpeakerDataManager(
+    SpeakerData,
+  );
+
+  const handleChangeSaturday = () => {
+    setSpeakingSaturday(!speakingSaturday);
+  };
+  const handleChangeSunday = () => {
+    setSpeakingSunday(!speakingSunday);
+  };
+  const heartFavoriteHandler = useCallback((e, speakerRec) => {
+    e.preventDefault();
+    updateSpeakerRecord(speakerRec);
+  }, []);
+
+  const newSpeakerList = useMemo(
+    () =>
+      speakerList
+        .filter(
+          ({ sat, sun }) =>
+            (speakingSaturday && sat) || (speakingSunday && sun),
+        )
+        .sort(function (a, b) {
+          if (a.firstName < b.firstName) {
+            return -1;
+          }
+          if (a.firstName > b.firstName) {
+            return 1;
+          }
+          return 0;
+        }),
+    [speakingSaturday, speakingSunday, speakerList],
+  );
+
+  const speakerListFiltered = isLoading ? [] : newSpeakerList;
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <div className="container">
+        <div className="btn-toolbar  margintopbottom5 checkbox-bigger">
+          {context.showSpeakerSpeakingDays === false ? null : (
+            <div className="hide">
+              <div className="form-check-inline">
+                <label className="form-check-label">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    onChange={handleChangeSaturday}
+                    checked={speakingSaturday}
+                  />
+                  Saturday Speakers
+                </label>
+              </div>
+              <div className="form-check-inline">
+                <label className="form-check-label">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    onChange={handleChangeSunday}
+                    checked={speakingSunday}
+                  />
+                  Sunday Speakers
+                </label>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="row">
+          <div className="card-deck">
+            {speakerListFiltered.map(
+              ({ id, firstName, lastName, bio, sat, sun, favorite }) => {
+                return (
+                  <SpeakerDetail
+                    key={id}
+                    id={id}
+                    favorite={favorite}
+                    onHeartFavoriteHandler={heartFavoriteHandler}
+                    firstName={firstName}
+                    lastName={lastName}
+                    bio={bio}
+                    sat={sat}
+                    sun={sun}
+                  />
+                );
+              },
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Speakers;
