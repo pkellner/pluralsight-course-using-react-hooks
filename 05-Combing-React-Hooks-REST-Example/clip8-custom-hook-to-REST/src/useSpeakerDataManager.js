661704f3a3deb18de235015fa83107b5f7878067
pkellner <peter@peterkellner.net>
2020-07-31 13:30:08 -0700

    done thru m5

--- 05-Combing-React-Hooks-REST-Example/clip8-custom-hook-to-REST/src/useSpeakerDataManager.js
+++ 05-Combing-React-Hooks-REST-Example/clip8-custom-hook-to-REST/src/useSpeakerDataManager.js
@@ -1,6 +1,7 @@
 import speakersReducer from './speakersReducer';
 import SpeakerData from './SpeakerData';
 import { useEffect, useReducer } from 'react';
+import axios from 'axios';
 
 function useSpeakerDataManager() {
   const [{ isLoading, speakerList }, dispatch] = useReducer(speakersReducer, {
@@ -9,19 +10,30 @@ function useSpeakerDataManager() {
   });
  
   function toggleSpeakerFavorite(speakerRec) {
-    speakerRec.favorite === true
-      ? dispatch({ type: 'unfavorite', id: speakerRec.id })
-      : displatch({ type: 'favorite', id: speakerRec.id });
+    const updateData = async function() {
+      axios.put(`http://localhost:4000/speakers/${speakerRec.id}`, speakerRec);
+      speakerRec.favorite === true
+        ? dispatch({ type: 'unfavorite', id: speakerRec.id })
+        : dispatch({ type: 'favorite', id: speakerRec.id });
+    };
+    updateData();
   }
   useEffect(() => {
-    new Promise(function (resolve) {
-      setTimeout(function () {resolve();}, 1000);
-    }).then(() => {
-      dispatch({
-        type: 'setSpeakerList',
-        data: SpeakerData,
-      });
-    });
+    // new Promise(function (resolve) {
+    //   setTimeout(function () {resolve();}, 1000);
+    // }).then(() => {
+    //   dispatch({
+    //     type: 'setSpeakerList',
+    //     data: SpeakerData,
+    //   });
+    // });
+    const fetchData = async function() {
+      let result = await axios.get
+      ('http://localhost:4000/speakers');
+      dispatch({ type: 'setSpeakerList', data: result.data });
+    };
+    fetchData();
+    
     return () => {
       console.log('cleanup');
     };
