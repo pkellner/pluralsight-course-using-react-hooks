800fee1f39f5143695d662d311dc828dc49ca139
pkellner <peter@peterkellner.net>
2020-08-02 09:00:37 -0700

    wip

--- 04-React-Classes-Differences-To-Hooks/clip4-useRef-And-createRef/src/ImageToggleOnMouseOverHook.js
+++ 04-React-Classes-Differences-To-Hooks/clip4-useRef-And-createRef/src/ImageToggleOnMouseOverHook.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, {useRef} from 'react';
 
 const ImageToggleOnMouseOverHook = ({ primaryImg, secondaryImg }) => {
   const imageRef = useRef(null);
