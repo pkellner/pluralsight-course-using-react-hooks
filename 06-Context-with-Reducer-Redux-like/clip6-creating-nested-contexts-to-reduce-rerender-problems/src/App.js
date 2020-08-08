2b3a6c51f1dc7818fc22e2fee794079653085157
pkellner <peter@peterkellner.net>
2020-08-08 14:13:02 -0700

    wip

--- 06-Context-with-Reducer-Redux-like/clip6-creating-nested-contexts-to-reduce-rerender-problems/src/App.js
+++ 06-Context-with-Reducer-Redux-like/clip6-creating-nested-contexts-to-reduce-rerender-problems/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './Home';
 import Speakers from './Speakers';
 import { GlobalProvider } from './GlobalState';
+import { FavoriteClickCountProvider } from './FavoriteClickCountContext';
 
 export const ConfigContext = React.createContext();
 
@@ -20,7 +21,9 @@ const App = ({ pageName }) => {
   return (
     <ConfigContext.Provider value={configValue}>
       <GlobalProvider>
-        <div>{pageToShow(pageName)}</div>
+        <FavoriteClickCountProvider>
+          <div>{pageToShow(pageName)}</div>
+        </FavoriteClickCountProvider>
       </GlobalProvider>
     </ConfigContext.Provider>
   );
