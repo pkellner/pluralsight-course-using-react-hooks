7fc49a13a411f9ee8b9ff14123a17369a1b92991
pkellner <peter@peterkellner.net>
2020-12-02 06:55:25 -0800

    Small Bug Fix

---
+++ 99-patches/03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip10-useMemo-to-Cache-Speaker-Sorted-Data/src/SpeakerDetail.js
@@ -0,0 +1,37 @@
+import ImageToggleOnScroll from './ImageToggleOnScroll';
+
+const SpeakerDetail = React.memo(
+  ({ id, firstName, lastName, favorite, bio, onHeartFavoriteHandler, dayChangeCount }) => {
+    console.log(`SpeakerDetail:${id} ${firstName} ${lastName} ${favorite}`);
+
+    return (
+      <div className="card col-4 cardmin">
+        <ImageToggleOnScroll
+          className="card-img-top"
+          primaryImg={`/static/speakers/bw/Speaker-${id}.jpg`}
+          secondaryImg={`/static/speakers/Speaker-${id}.jpg`}
+          alt="{firstName} {lastName}"
+          dayChangeCount={dayChangeCount}
+        />
+        <div className="card-body">
+          <h4 className="card-title">
+            <button
+              data-sessionid={id}
+              className={favorite ? 'heartredbutton' : 'heartdarkbutton'}
+              onClick={(e) => {
+                onHeartFavoriteHandler(e, !favorite);
+              }}
+            />
+            <span>
+              {firstName} {lastName}
+            </span>
+          </h4>
+
+          <span>{bio}</span>
+        </div>
+      </div>
+    );
+  },
+);
+
+export default SpeakerDetail;
---
+++ 99-patches/03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip10-useMemo-to-Cache-Speaker-Sorted-Data/src/speakersReducer.js
@@ -0,0 +1,24 @@
+const speakersReducer = (state, action) => {
+  function updateFavorite(favoriteValue) {
+    return state.map((item, index) => {
+      if (item.id === action.sessionId) {
+        return { ...item, favorite: favoriteValue };
+      }
+      return item;
+    });
+  }
+  switch (action.type) {
+    case 'setSpeakerList': {
+      return action.data;
+    }
+    case 'favorite': {
+      return updateFavorite(true);
+    }
+    case 'unfavorite': {
+      return updateFavorite(false);
+    }
+    default:
+      return state;
+  }
+};
+export default speakersReducer;
