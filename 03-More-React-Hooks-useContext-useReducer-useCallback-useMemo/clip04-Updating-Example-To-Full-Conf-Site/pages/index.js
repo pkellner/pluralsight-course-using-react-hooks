8b5c07d447857fcf38d51e3b32a0898748c4856a
pkellner <peter@peterkellner.net>
2021-04-05 07:00:21 -0700

    working on re-doing full conf website

--- 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip04-Updating-Example-To-Full-Conf-Site/pages/index.js
+++ 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip04-Updating-Example-To-Full-Conf-Site/pages/index.js
@@ -1,33 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import App from '../src/App.js';
 
-const InputElement = () => {
-  const random_boolean = Math.random() >= 0.5;
-  //const [isLoading,setIsLoading] = useState(true);
+function index() {
+  return <App pageName="Home" />;
+}
 
-  // (this will cause a random warning in console debug messages because server-side and client side both have random
-  //   number generation.  Just ignore the error as it does not effect the point we are making here with the React hooks rule).
-  //     Warning: Expected server HTML to contain a matching <input> in <div>.
-  const [isLoading, setIsLoading] = useState(random_boolean === true);
-
-  // if (random_boolean === true) {
-  //   const [isLoading,setIsLoading] = useState(true);
-  // } else {
-  //   const [isLoading,setIsLoading] = useState(false);
-  // }
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  });
-
-  return isLoading ? (
-    <div>Loading...</div>
-  ) : (
-    <div>
-      <input placeholder="Enter Some Text" />
-    </div>
-  );
-};
-
-export default InputElement;
+export default index;
